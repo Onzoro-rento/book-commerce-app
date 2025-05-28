2f56fdf2c8c6cd39e914abac98f75a12cf89b689
Onzoro-rento <rento0725ken1009@icloud.com>
2025-05-28 17:39:32 +0900

    test

--- app/api/checkout/success/route.ts
+++ app/api/checkout/success/route.ts
@@ -4,7 +4,7 @@ import prisma from "@/app/lib/prisma";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
-export async function POST(request: Request, Response: Response) {
+export async function POST(request: Request) {
 
     const {sessionId} = await request.json();
     try {
--- app/api/purchases/[userId]/route.ts
+++ app/api/purchases/[userId]/route.ts
@@ -1,17 +1,21 @@
-import { NextResponse } from "next/server";
 import prisma from "@/app/lib/prisma";
-// 購入履歴の取得API
-export async function GET(request: Request, { params }: { params: { userId: string } }
+import { NextResponse } from "next/server";
 
+//https://nextjs.org/docs/app/building-your-application/routing/route-handlers#dynamic-route-segments
+export async function GET(
+  request: Request,
+  { params }: { params: { userId: string } }
 ) {
-    const userId = params.userId;
-    try {
-        const purchases = await prisma.purchase.findMany({
-            where: {userId: userId},
-        });
-        return NextResponse.json(purchases);
-    } catch (err) {
-        return NextResponse.json(err);
-    }
-    
+  const userId = params.userId;
+
+  try {
+    const purchase = await prisma.purchase.findMany({
+      where: { userId: userId },
+    });
+    console.log(purchase);
+
+    return NextResponse.json(purchase);
+  } catch (err) {
+    return NextResponse.json(err);
+  }
 }
